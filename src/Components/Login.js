d1ef6de0b5cc3f54b1fa0236d307b8da0a0f6568
Hannah Thorpe <hthorpe@protonmail.com>
2020-05-26 12:27:07 -0400

    login, register, reset, not found wireframe

---
+++ src/Components/Login.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Login extends React.Component {
+render() {
+  return (
+    <div className='login'>
+      <h1>Log In</h1>
+        <p>Welcome back!</p>
+        <form type='submit' for='account-login'>
+          <input type="text" placeholder="Username" />
+          <input type="text" placeholder="Password" />
+          <button onClick={() => this.props.history.push('/Dashboard')}>Log In</button>
+        </form>
+    </div>
+  )
+}
+}
+
+export default Login;
---
+++ src/Components/NotFound.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class NotFound extends Component {
+  render() {
+    return (
+      <section>
+        <h2>404 - Page not found</h2>
+        <p>Try going back to your previous page.</p>
+      </section>
+    );
+  }
+}
+
+export default NotFound;
