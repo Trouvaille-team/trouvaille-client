d361c2469256418b7af479fecbffb545914849f3
MediocreIan <idrews094@gmail.com>
2020-05-27 13:54:32 -0500

    moving work to server side

---
+++ src/Components/waypoints-select/Waypoint-select.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class WaypointSelect extends React.Component {
+    constructor(props) {
+    super(props);
+    this.state = {
+      currentWaypoint: {}
+
+    }
+  }
+}
