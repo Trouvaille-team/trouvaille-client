e6bfc16f9581f8475fbffd9bf030d20d3dbaa6b6
Zac Barreca <zacbarreca@gmail.com>
2020-05-27 10:51:33 -0400

    add render test

---
+++ src/Components/NotFound/NotFound.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import NotFound from './NotFound';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(
+    <BrowserRouter>
+      <NotFound />
+    </BrowserRouter>,
+    div
+  );
+  ReactDOM.unmountComponentAtNode(div);
+});
