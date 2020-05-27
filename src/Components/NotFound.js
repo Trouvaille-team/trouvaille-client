7915e28f4897af3e82d59169d4e9b3ff322900f3
Douglas Miner <curr3nas@gmail.com>
2020-05-27 14:53:00 -0500

    put back some files that got deleted by accident

---
+++ src/Components/NotFound.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class NotFound extends Component {
+  render() {
+    return (
+      <section>
+        <h2>404 - Page not found</h2>
+        <p>
+          I know we said that spontaneity and unplanned wandering are the best things in life, but nothing is here! Maybe you should head back to the previous page and try again.
+        </p>
+      </section>
+    );
+  }
+}
+
+export default NotFound;
---
+++ src/Components/Register.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+class Register extends React.Component{
+  render() {
+    return (
+      <div className='register'>
+        <h1>Register</h1>
+        <p>“I may not have gone where I intended to go, 
+          but I think I have ended up where I intended to be.”
+        </p>
+        <p>
+         - Douglas Adams
+        </p>
+        <div>
+          <p>
+            Ready to discover new places? Sign up today!
+          </p>
+        </div>
+        <form type='submit' for='account-sign-up'>
+          <input type="text" placeholder='Username' />
+          <input type="email" placeholder='Email' />
+          <input type="password" placeholder="Password" />
+          <button type='submit'>Sign Up</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default Register;
