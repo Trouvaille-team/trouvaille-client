dbdce1131b6619218c0fe56d085516e18f185575
Trina <trinamceary@gmail.com>
2020-05-28 09:45:37 -0700

    add submit btn to plan trip page

--- src/Components/Nav/PlanTrip.js
+++ src/Components/Nav/PlanTrip.js
@@ -2,18 +2,25 @@ import React from 'react';
 import Menu from '../Menu/Menu'
 
 class PlanTrip extends React.Component{
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log('form submitted')
+    //api post request in here...
+    //to what endpoint?
+  }
+
   render() {
     return (
       <>
         <Menu />
-        <form>
-          <label for='destination'>Where are you going?</label>
+        <form onSubmit={ e => this.handleSubmit(e)}>
+          <label htmlFor='destination'>Where are you going?</label>
           <input type='text' placeholder='city, state, or zip' required/>
 
-          <label for='num-detours'>How many detours will you make en route?</label>
+          <label htmlFor='num-detours'>How many detours will you make en route?</label>
           <input type='number' id='num-detours' min='1' max='25?'/>
 
-          <label for='radius'>Choose your max detour radius</label>
+          <label htmlFor='radius'>Choose your max detour radius</label>
           <select name='radius' id='radius'>
             <option>0-15 miles</option>
             <option>15-30 miles</option>
@@ -22,8 +29,9 @@ class PlanTrip extends React.Component{
           </select>
 
           {/* assuming max time includes entire trip start to finish */}
-          <label for='time'>Choose your max trip time(hrs)</label> 
-          <input type='number' id='time' min='1' max='100'/>   
+          <label htmlFor='time'>Choose your max trip time(hrs)</label> 
+          <input type='number' id='time' min='1' max='100'/>  
+          <button type='submit'>Submit</button>
         </form>
       </>
     )
