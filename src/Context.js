b611de1c2c33ed69884be5c6b4624b391aaf42cb
Trina <trinamceary@gmail.com>
2020-05-29 16:42:14 -0700

    can add checked items to interests array...but multiple times

--- src/Context.js
+++ src/Context.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 
 const Context = React.createContext({
-  showMenu: false,
-  toggleMenu: ()=>{},
+  showMenu: false, 
+  userInterests: [],
   userTrip: {
     destination: '',
     numDetours: 0,
     maxRadius: 0,
     maxTime: 0
   },
+  toggleMenu: ()=>{},
+  setUserInterests: ()=>{},
   setTrip: ()=>{}
 })
 export default Context
@@ -18,13 +20,15 @@ export default Context
 export class ContextProvider extends Component {
   state ={
     showMenu: false,
-    toggleMenu: ()=>{},
+    userInterests: [],
     userTrip: {
       destination: '',
       numDetours: null,
       maxRadius: null,
       maxTime: null
-    },
+    }, 
+    toggleMenu: ()=>{}, 
+    setUserInterests: ()=>{},
     setTrip: ()=>{}
   }
 
@@ -32,7 +36,16 @@ export class ContextProvider extends Component {
     this.setState({ showMenu: !this.state.showMenu})
   }
 
+  //Add items to interests array
+  //need to prevent duplicates from being added
+  //need to remove items when they're unchecked
+    //Store in database if user is logged in
+  setUserInterests = (newInterest) => {
+    this.setState({ userInterests: [...this.state.userInterests, newInterest]})
+  }
+
   //Store all all the values passed into the PlanTrip form!
+      //Store in database if user is logged in
   setTrip = (destination, detours, radius, time) => {
     this.setState({ userTrip: {
       destination: destination,
@@ -45,8 +58,10 @@ export class ContextProvider extends Component {
   render() {
     const value = {
       showMenu: this.state.showMenu,
-      toggleMenu: this.toggleMenu,
+      userInterests: [],
       userTrip: this.state.userTrip,
+      toggleMenu: this.toggleMenu,
+      setUserInterests: this.setUserInterests,
       setTrip: this.setTrip
     }
     return (
--- src/Interests/Interests.js
+++ src/Interests/Interests.js
@@ -1,6 +1,28 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import ContextProvider from '../Context'
 
 export default class Interests extends Component {
+    // componentDidUpdate() {
+    //     console.log(this.context)
+    // }
+
+    static contextType = ContextProvider
+
+    handleCheck = (e) => {
+        let value = e.target.id;
+        let checked = e.target.checked;
+        console.log('value:', value)
+        console.log('checked:', checked)
+        if (checked) {
+            this.context.setUserInterests(value);
+        }
+    }
+
+    handleSubmit = e => {
+        e.preventDefault();
+        console.log('submitted')
+        console.log(this.state.interests)
+    }
     
 
     render() {
@@ -14,35 +36,35 @@ export default class Interests extends Component {
                     </section>
                 </header>
                 <div>
-                    <form className="interests-form">
+                    <form className="interests-form" onSubmit={e=>this.handleSubmit(e)}>
                         <label htmlFor="camping">Camping</label>
-                        <input id="camping" type="checkbox" ></input>
+                        <input id="camping" type="checkbox" onClick={e=>this.handleCheck(e)}></input>
                         <label htmlFor="hiking">Hiking</label>
-                        <input id="hiking" type="checkbox" ></input>
+                        <input id="hiking" type="checkbox" onClick={e=>this.handleCheck(e)}></input>
                         <label htmlFor="beaches">Beaches</label>
-                        <input id="beaches" type="checkbox"></input>
+                        <input id="beaches" type="checkbox" onClick={e=>this.handleCheck(e)}></input>
                         <label htmlFor="breweries">Breweries/Wineries</label>
-                        <input id="breweries" type="checkbox"></input>
+                        <input id="breweries" type="checkbox" onClick={e=>this.handleCheck(e)}></input>
                         <label htmlFor="museums">Museums</label>
-                        <input id="museums" type="checkbox"></input>
+                        <input id="museums" type="checkbox" onClick={e=>this.handleCheck(e)}></input>
                         <label htmlFor="novelty">Novelty</label>
-                        <input id="novelty" type="checkbox"></input>
+                        <input id="novelty" type="checkbox" onClick={e=>this.handleCheck(e)}></input>
                         <label htmlFor="military">Military/Memorials/Battlefields</label>
-                        <input id="military" type="checkbox"></input>
+                        <input id="military" type="checkbox" onClick={e=>this.handleCheck(e)}></input>
                         <label htmlFor="monuments">Monuments/Landmarks</label>
-                        <input id="monuments" type="checkbox"></input>
+                        <input id="monuments" type="checkbox" onClick={e=>this.handleCheck(e)}></input>
                         <label htmlFor="parks">Parks</label>
-                        <input id="parks" type="checkbox"></input>
+                        <input id="parks" type="checkbox" onClick={e=>this.handleCheck(e)}></input>
                         <label htmlFor="zoos">Zoos</label>
-                        <input id="zoo" type="checkbox"></input>
+                        <input id="zoo" type="checkbox" onClick={e=>this.handleCheck(e)}></input>
                         <label htmlFor="amusement">Amusement Parks</label>
-                        <input id="amusement" type="checkbox"></input>                        
+                        <input id="amusement" type="checkbox" onClick={e=>this.handleCheck(e)}></input>                        
                         <label htmlFor="haunted">Haunted</label>
-                        <input id="haunted" type="checkbox"></input>
+                        <input id="haunted" type="checkbox" onClick={e=>this.handleCheck(e)}></input>
                         <div>
                             <button 
-                            type='submit'
-                            onClick={() => this.props.history.push('/dashboard')}
+                            // type='submit'
+                            // onClick={() => this.props.history.push('/dashboard')}
                             >Submit</button>
                         </div>
                     </form>
