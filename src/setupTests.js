8dd2dbfe2cf41f717d429d2b11d7b1bfcfee2c66
Douglas Miner <curr3nas@gmail.com>
2020-06-15 09:17:14 -0500

    removed unneeded commented code

--- src/setupTests.js
+++ src/setupTests.js
@@ -11,40 +11,5 @@ require('jest-canvas-mock')
 configure({ adapter: new Adapter() });
 const mockGeolocation = { getCurrentPosition: jest.fn(), watchPosition: jest.fn() };
 
-const mockContext = {
-  "value": {
-    "user": {
-      "id": 2,
-      "username": "ian"
-    },
-    "showMenu": false,
-    "userInterests": [
-      "Camping",
-      "Monuments",
-      "Parks"
-    ],
-    "userTrip": {
-      "destination": "Chicago",
-      "numDetours": "",
-      "maxRadius": 6000,
-      "maxTime": ""
-    },
-    "toggleMenu": "ƒ () {}",
-    "addUserInterests": "ƒ () {}",
-    "removeUserInterests": "ƒ () {}",
-    "clearUserInterests": "ƒ () {}",
-    "setTrip": "ƒ () {}",
-    "waypoints": "[{…}, {…}, {…}]",
-    "setWaypoints": "ƒ () {}",
-    "setEndCoords": "ƒ () {}",
-    "endCoords": "{lat: 41.8781139, lng: -87.6297872}",
-    "setOriginCoords": "ƒ () {}",
-    "originCoords": "{lat: 43.030898699999995, lng: -87.98246619999999}",
-    "processLogin": "ƒ () {}",
-    "setUser": "ƒ () {}",
-    "processLogout": "ƒ () {}",
-    "setRadius": "ƒ () {}",
-    "radius": 6000
-  }
-}
+
 global.navigator.geolocation = mockGeolocation;
